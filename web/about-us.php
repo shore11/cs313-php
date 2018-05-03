c142eee0446650410a9b72a9ebeed5df831a2bb6
Aaron <twin11shore@gmail.com>
2018-05-03 16:40:43 -0600

    nav

--- web/about-us.php
+++ web/about-us.php
@@ -2,6 +2,7 @@
 
 <html>
     <head>
+        <link rel="stylesheet" type="text/css" href="nav.css">
     </head>
     <body>
         <?php
---
+++ web/login.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <link rel="stylesheet" type="text/css" href="nav.css">
+    </head>
+    <body>
+        <?php
+            require("navbar.php");
+        ?>
+    </body>
+</html>
