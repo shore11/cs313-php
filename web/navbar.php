c733d5b6c4762cce7989938d9875f8ad1e670ded
Aaron <twin11shore@gmail.com>
2018-05-03 16:37:12 -0600

    nav

---
+++ web/navbar.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <link rel="stylesheet" type="text/css" href="nav.css">
+    </head>
+    <body>
+        <ul>
+            <?php
+            $url = $_SERVER['REQUEST_URI'];
+            $end = end(explode('/', $url));
+            $class1 = "";
+            $class2 = "";
+            $class3 = "";
+            if ($end == 'about-us.php'){
+                $class1 = "active";
+            } elseif ($end == 'login.php'){
+                $class3 = "active";
+            } elseif ($end == 'home.php'){
+                $class2 = "active";
+            }
+            ?>
+            <li class= <?php echo $class1; ?> ><a href='about-us.php'>About Us</a></li>
+            <li class= <?php echo $class2; ?> ><a href='home.php'>Home</a></li>
+            <li class= <?php echo $class3; ?> ><a href='login.php'>Login</a></li>
+            
+            </ul>
+    </body>
+</html>
