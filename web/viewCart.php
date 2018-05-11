b5eaf23e8cc4b257180bc8be72b0f398a2d27ab4
Aaron <twin11shore@gmail.com>
2018-05-11 16:33:01 -0600

    php test

--- web/viewCart.php
+++ web/viewCart.php
@@ -6,9 +6,9 @@
 <html>
     <body>
         <p><?php
-            var_dump($_SESSION['cart']);
-            print_r($_SESSION['cart']);
-            echo $_SESSION['cart'][0];
+            //var_dump($_SESSION['cart']);
+            //print_r($_SESSION['cart']);
+            echo $_SESSION['cart'][0][1];
             ?></p>
     </body>
 </html>
