68c4c141c140ad19fa250846bfa58aad205bc221
Aaron <twin11shore@gmail.com>
2018-05-10 19:36:19 -0600

    3 check error

---
+++ web/prove3.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="prove3.css">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+        <title>Browse</title>
+        <link rel="icon" href="rims.png"/>
+    </head>
+    <body class="back">
+        <header>Every thing you need to be ready for the disco!</header>
+        <!-- row 1 -->
+        <div class="row"><div class="col-md-4"><div class="col-md-12">
+                    hello
+                </div></div>
+            <div class="col-md-4"><div class="col-md-12">
+                    hello
+                </div></div>
+            <div class="col-md-4"><div class="col-md-12">
+                    hello
+                </div></div></div>
+    <!-- row 2 -->
+        <div class="row"><div class="col-md-4"><div class="col-md-12">
+                    hello
+                </div></div>
+            <div class="col-md-4"><div class="col-md-12">
+                    hello
+                </div></div>
+            <div class="col-md-4"><div class="col-md-12">
+                    hello
+                </div></div></div>
+        <!-- row 3 -->
+        <div class="row"><div class="col-md-4"><div class="col-md-12">
+                    hello
+                </div></div>
+            <div class="col-md-4"><div class="col-md-12">
+                    hello
+                </div></div>
+            <div class="col-md-4"><div class="col-md-12">
+                    hello
+                </div></div></div>
+         <!-- row 4 -->
+        <div class="row"><div class="col-md-4"><div class="col-md-12">
+                    hello
+                </div></div>
+            <div class="col-md-4"><div class="col-md-12">
+                    hello
+                </div></div>
+            <div class="col-md-4"><div class="col-md-12">
+                    hello
+                </div></div></div>
+    </body>
+</html>
