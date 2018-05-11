b3dfaa454292f8a9d729c743315b3d79ac49977a
Aaron <twin11shore@gmail.com>
2018-05-11 15:32:57 -0600

    php test

--- web/add.php
+++ web/add.php
@@ -1,11 +1,11 @@
 <?php
     session_start();
-    $item = $_POST["item"];
-    $price = $_POST["price"];
+    $item = $_POST['item'];
+    $price = $_POST['price'];
 
     // a bag has an item and its price
     //$bag = array($item, $price);
-    array_push($_SESSION['cart'], $item, $price);
+    array_push($_SESSION['cart'],$item,$price);
 
     echo '<script>window.location.href="prove3.php";</script>';
    // exit();
