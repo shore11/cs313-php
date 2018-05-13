e06ca070b1dbd2fa6f04a8cd439a730de6180ab1
Aaron <twin11shore@gmail.com>
2018-05-12 20:38:25 -0600

    final

--- web/prove3.php
+++ web/prove3.php
@@ -16,80 +16,94 @@
         <div class="container">
         <!-- row 1 -->
         <div class="row"><div class="col-lg-4"><div class="col-lg-12">
-            hello Moto
-            <p>asdfsaf You wher</p>
-            <p>as you where</p>
+            <img class="picture" src="pics/boombox.jpg">
         <!-- BUTTON -->
             <form method="post" action="add.php" >
-                <input type="hidden" name="item" value"">
+                <p>Boom Box Price: $50.00</p>
+                <input type="hidden" name="price" value="50.00">
+                <input type="hidden" name="item" value="Boom Box">
                 <input class="sub" type="submit" value="+ Add To Cart">
             </form>
                 </div></div>
             <div class="col-lg-4"><div class="col-lg-12">
-                    hello
+                    <img class="picture" src="pics/headband.jpg">
         <!-- BUTTON -->
             <form method="post" action="add.php" >
-                <input type="hidden" name="item" value"">
+                <p>Head Band Price: $5.43</p>
+                <input type="hidden" name="price" value="5.43">
+                <input type="hidden" name="item" value="Head Band">
                 <input class="sub" type="submit" value="+ Add To Cart">
             </form>
                 </div></div>
             <div class="col-lg-4"><div class="col-lg-12">
-                    hello
+                   <img class="picture" src="pics/earings.jpg">
         <!-- BUTTON -->
             <form method="post" action="add.php" >
-                <input type="hidden" name="item" value"">
+                <p>Hoop Earings Price: $1.33</p>
+                <input type="hidden" name="price" value="1.33">
+                <input type="hidden" name="item" value="Hoop Earings">
                 <input class="sub" type="submit" value="+ Add To Cart">
             </form>
                 </div></div></div>
     <!-- row 2 -->
         <div class="row"><div class="col-lg-4"><div class="col-lg-12">
-                    hello
+                    <img class="picture" src="pics/glasses.jpg">
         <!-- BUTTON -->
             <form method="post" action="add.php" >
-                <input type="hidden" name="item" value"">
+                <p>Glasses Price: $2.33</p>
+                <input type="hidden" name="price" value="2.33">
+                <input type="hidden" name="item" value="Glasses">
                 <input class="sub" type="submit" value="+ Add To Cart">
             </form>
                 </div></div>
             <div class="col-lg-4"><div class="col-lg-12">
-                    hello
+                    <img class="picture" src="pics/shirt.jpg">
         <!-- BUTTON -->
             <form method="post" action="add.php" >
-                <input type="hidden" name="item" value"">
+                <p>Shirt Price: $8.75</p>
+                <input type="hidden" name="price" value="8.75">
+                <input type="hidden" name="item" value="Shirt">
                 <input class="sub" type="submit" value="+ Add To Cart">
             </form>
                 </div></div>
             <div class="col-lg-4"><div class="col-lg-12">
-                    hello
+                   <img class="picture" src="pics/bellPants.jpg">
          <!-- BUTTON -->
             <form method="post" action="add.php" >
-                <input type="hidden" name="item" value"">
+                <p>Bell Bottom Pants Price: $10.25</p>
+                <input type="hidden" name="price" value="10.25">
+                <input type="hidden" name="item" value="Bell Bottom Pants">
                 <input class="sub" type="submit" value="+ Add To Cart">
             </form>        
                 </div></div></div>
         <!-- row 3 -->
         <div class="row"><div class="col-lg-4"><div class="col-lg-12">
-                    hello
+                  <img class="picture" src="pics/discoBall.jpg">
         <!-- BUTTON -->
             <form method="post" action="add.php" >
-                <input type="hidden" name="item" value"">
+                <p>Disco Ball Price: $30.00</p>
+                <input type="hidden" name="price" value="30.00">
+                <input type="hidden" name="item" value="Disco Ball">
                 <input class="sub" type="submit" value="+ Add To Cart">
             </form>
                 </div></div>
             <div class="col-lg-4"><div class="col-lg-12">
-                    hello
+                    <img class="picture" src="pics/rollerblades.jpg">
         <!-- BUTTON -->
             <form method="post" action="add.php" >
-                <input type="hidden" name="item" value"">
+                <p>Roller Blades Price: $25.00</p>
+                <input type="hidden" name="price" value="25.00">
+                <input type="hidden" name="item" value="Roller Blades">
                 <input class="sub" type="submit" value="+ Add To Cart">
             </form>
                 </div></div>
             <div class="col-lg-4"><div class="col-lg-12">
-                    hello
+                    <img class="picture" src="pics/afro.jpg">
         <!-- BUTTON -->
             <form method="post" action="add.php" >
-                <p>Price: $9.11</p>
+                <p>Afro Price: $9.11</p>
                 <input type="hidden" name="price" value="9.11">
-                <input type="hidden" name="item" value="afro">
+                <input type="hidden" name="item" value="Afro">
                 <input class="sub" type="submit" value="+ Add To Cart">
             </form>
                 </div></div></div>
