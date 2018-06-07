57c27bd464bd01aa8bdd4d4c2be33ee805d0142b
Aaron <twin11shore@gmail.com>
2018-06-07 17:29:53 -0600

    test vendor info

--- web/moreInfo.php
+++ web/moreInfo.php
@@ -1,8 +1,48 @@
 <!DOCTYPE html>
+<?php
+    $dbUrl = getenv('DATABASE_URL');
+    $dbopts = parse_url($dbUrl);
+
+    $dbHost = $dbopts["host"];
+    $dbPort = $dbopts["port"];
+    $dbUser = $dbopts["user"];
+    $dbPassword = $dbopts["pass"];
+    $dbName = ltrim($dbopts["path"],'/');
+
+    $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+?>
 <html>
     <head>
+        <title>More Info</title>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+        <link rel="stylesheet" href="moreInfo.css">
     </head>
-    <body>
-        <h1>COMING SOON!</h1>
-    </body>
+    <body style="background-color: lightgray">
+<div class="container">
+  <div class="row" >
+    <div class="col-sm-2"></div>
+    <div class="col-sm-8 vendor">
+            <div class="row">
+                <?php
+                    $search =$_GET["id"];
+                    $query = "SELECT v.first_name, v.last_name, v.phone_number, v.email FROM vendor v WHERE v.vendor_id = :search";
+                    $stmt = $db->prepare($query);
+                    $stmt->bindValue(":search", $search, PDO::PARAM_INT);
+                    $stmt->execute();
+                    $result = $stmt->fetch(PDO::FETCH_ASSOC);
+                    
+                    $fname = $result["first_name"];
+                    echo "<div class='col-sm-4'>$fname</div>";
+                    echo "<div class='col-sm-4'>Name</div>";
+                    echo "<div class='col-sm-4'>Name</div>";
+                ?>
+            </div>
+      </div>
+    <div class="col-sm-2"></div>
+  </div>
+</div>
+</body>
 </html>
